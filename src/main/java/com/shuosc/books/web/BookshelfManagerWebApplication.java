714e08d1848e60303faaf0fb9c6e17fe34257176
钟坤林 <zkl@zkllab.com>
2021-01-14 15:26:04 +0800

    chore: initial commit
    [skip ci]

---
+++ src/main/java/com/shuosc/books/web/BookshelfManagerWebApplication.java
@@ -0,0 +1,13 @@
+package com.shuosc.books.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BookshelfManagerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshelfManagerWebApplication.class, args);
+    }
+}
---
+++ src/test/java/com/shuosc/books/web/BookshelfManagerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuosc.books.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BookshelfManagerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
