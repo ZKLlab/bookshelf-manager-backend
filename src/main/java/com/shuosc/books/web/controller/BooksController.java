6c993d8c223d2d82a43a69e26fde7398174e4f74
钟坤林 <zkl@zkllab.com>
2021-03-04 10:08:32 +0800

    refactor: books

--- src/main/java/com/shuosc/books/web/controller/BooksController.java
+++ src/main/java/com/shuosc/books/web/controller/BooksController.java
@@ -1,5 +1,6 @@
 package com.shuosc.books.web.controller;
 
+import com.shuosc.books.web.model.Book;
 import com.shuosc.books.web.model.Return;
 import com.shuosc.books.web.service.BookService;
 import com.shuosc.books.web.service.HoldingService;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -24,13 +28,25 @@ public class BooksController {
 
     @GetMapping(path = "/books")
     public Return listBooks() {
-        return Return.success("查询成功", bookService.findAll());
+        var books = bookService.findAll().stream()
+                .filter(Book::getVisible)
+                .sorted((o1, o2) -> {
+                    var a = o1.getPublicationDate().isEmpty()
+                            ? o1.getPublicationDate().get(0)
+                            : 0;
+                    var b = o2.getPublicationDate().isEmpty()
+                            ? o2.getPublicationDate().get(0)
+                            : 0;
+                    return b - a;
+                })
+                .collect(Collectors.toCollection(ArrayList::new));
+        return Return.success("查询成功", books);
     }
 
     @GetMapping(path = "/books/{id}")
     public Return getBook(@PathVariable String id) {
         var book = bookService.findById(id);
-        if (book == null)
+        if (book == null || !book.getVisible())
             return Return.failure("图书不存在，获取失败");
 
         return Return.success("获取成功", bookService.findById(id));
